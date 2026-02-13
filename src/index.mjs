364252d4a8873a87a03d15adf9ae0e6d6c920532
nikolaPetkovic96 <44683836+nikolaPetkovic96@users.noreply.github.com>
2026-02-13 23:28:51 +0100

    added aws_region to yml

--- src/index.mjs
+++ src/index.mjs
@@ -2,7 +2,7 @@ export const handler = async (event) => {
   // TODO implement
   const response = {
     statusCode: 200,
-    body: JSON.stringify('Hello from Lambda! - updated from vsCodium v3.2'),
+    body: JSON.stringify('Hello from Lambda! - updated from vsCodium v3.3'),
   };
   return response;
 };
