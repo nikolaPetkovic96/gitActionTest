383ec9978234c21b01efda5168f38d0db056eb6f
nikolaPetkovic96 <44683836+nikolaPetkovic96@users.noreply.github.com>
2026-02-13 23:47:25 +0100

    newreg2

--- src/index.mjs
+++ src/index.mjs
@@ -2,7 +2,7 @@ export const handler = async (event) => {
   // TODO implement
   const response = {
     statusCode: 200,
-    body: JSON.stringify('Hello from Lambda! - updated from vsCodium v3.33'),
+    body: JSON.stringify('Hello from Lambda! - updated from vsCodium v3.34'),
   };
   return response;
 };
